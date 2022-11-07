eaaac7bfb3edbe6e8bac8995c09f48a43d9fa8af
Linar <linar.adgemov@mail.ru>
2022-11-07 22:56:45 +0300

    initial comit

---
+++ RicAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RicAndMorty
+//
+//  Created by Linar Adgemov on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
